592384a673c93ced76a6e17963da0e96c14b57b0
Alexey Tabakman <a.tabakman@corp.mail.ru>
2017-03-15 15:34:28 +0300

    HT4 - filter by selected

--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -6,10 +6,20 @@ import accrdion from '../../decorators/accordion'
 import './style.css'
 
 class ArticleList extends Component {
+    isArticleVisible = (article) => {
+        const {from, to, selected} = this.props.filters
+        const selectedIds = selected.map(option => option.value)
+
+        if (selectedIds.length && !selectedIds.includes(article.id)) {
+            return false
+        }
+        return true
+    }
+
     render() {
         const {articles, toggleOpenItem, isItemOpened} = this.props
 
-        const articleComponents = articles.map(article => <li key={article.id}>
+        const articleComponents = articles.filter(this.isArticleVisible).map(article => <li key={article.id}>
             <Article article={article}
                      isOpen={isItemOpened(article.id)}
                      toggleOpen={toggleOpenItem(article.id)}
@@ -33,7 +43,8 @@ class ArticleList extends Component {
 const mapStateToProps = state => {
     console.log('---', 'connect, state = ', state)
     return {
-        articles: state.articles
+        articles: state.articles,
+        filters: state.filters
     }
 }
 
@@ -42,5 +53,6 @@ export default connect(mapStateToProps)(accrdion(ArticleList))
 ArticleList.propTypes = {
     articles: PropTypes.array.isRequired,
     isItemOpened: PropTypes.func.isRequired,
-    toggleOpenItem: PropTypes.func.isRequired
+    toggleOpenItem: PropTypes.func.isRequired,
+    filters: PropTypes.object.isRequired
 }
