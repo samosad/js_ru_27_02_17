96179f4812a8638789c4738b28319d3dbf63be13
Roman <kaboom.ua@gmail.com>
2017-03-22 22:53:21 +0200

    Update uuid.js

--- src/middlewares/uuid.js
+++ src/middlewares/uuid.js
@@ -2,6 +2,7 @@ import getUUID from 'uuid'
 import {ADD_COMMENT} from '../constants'
 
 export default store => next => action => {
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
     if (action.type === ADD_COMMENT) {
         const id = getUUID()
         console.log('---', 'uuid:', id)
