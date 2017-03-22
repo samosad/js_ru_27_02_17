85599e75b862911c1021e19bb018819b09dc1594
Alexey Tabakman <a.tabakman@corp.mail.ru>
2017-03-22 13:17:20 +0300

    HT5 - addComment action

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,4 +1,4 @@
-import {INCREMENT, DELETE_ARTICLE, CHANGE_DATE_RANGE, CHANGE_SELECTION} from '../constants'
+import {INCREMENT, DELETE_ARTICLE, CHANGE_DATE_RANGE, CHANGE_SELECTION, ADD_COMMENT} from '../constants'
 
 export function increment() {
     const action = {
@@ -7,7 +7,6 @@ export function increment() {
 
     return action
 }
-
 
 export function deleteArticle(id) {
     return {
@@ -27,5 +26,12 @@ export function changeSelection(selected) {
     return {
         type: CHANGE_SELECTION,
         payload: { selected }
+    }
+}
+
+export function addComment(comment) {
+    return {
+        type: ADD_COMMENT,
+        payload: comment
     }
 }
--- src/components/NewCommentForm.js
+++ src/components/NewCommentForm.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react'
+import {connect} from 'react-redux'
+import {addComment} from '../AC'
 
 class NewCommentForm extends Component {
     static propTypes = {
@@ -20,6 +22,7 @@ class NewCommentForm extends Component {
 
     handleSubmit = ev => {
         ev.preventDefault()
+        this.props.addComment({...this.state})
         this.setState({
             user: '',
             text: ''
@@ -28,7 +31,7 @@ class NewCommentForm extends Component {
 
     render() {
         return (
-            <form onSubmit = {this.handleSubmit}>
+            <form onSubmit={this.handleSubmit}>
                 comment: <input type="text" value={this.state.text} onChange = {this.handleChange('text')}/>
                 user: <input type="text" value={this.state.user} onChange = {this.handleChange('user')}/>
                 <input type = "submit"/>
@@ -42,4 +45,6 @@ const validators = {
     user: (text) => text.length < 10
 }
 
-export default NewCommentForm
+export default connect(null, {
+    addComment
+})(NewCommentForm)
