b9947f1284ae1114438f84d5b8d7facbb2682461
Alexey Tabakman <a.tabakman@corp.mail.ru>
2017-03-22 13:33:59 +0300

    HT5 - addComment reducer update

--- src/middlewares/uuid.js
+++ src/middlewares/uuid.js
@@ -1,8 +1,12 @@
 import getUUID from 'uuid'
+import {ADD_COMMENT} from '../constants'
 
 export default store => next => action => {
-    const uuid = getUUID()
-    console.log('---', 'uuid:', uuid)
-    next({...action, uuid})
-//    next({...action, newField: 'some'})
+    if (action.type === ADD_COMMENT) {
+        const id = getUUID()
+        console.log('---', 'uuid:', id)
+        next({ ...action, payload: { ...action.payload, id } })
+    } else {
+        next(action)
+    }
 }
--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,11 +1,12 @@
 import {normalizedComments} from '../fixtures'
-import {  } from '../constants'
+import {ADD_COMMENT} from '../constants'
 
 export default (comments = normalizedComments, action) => {
     const { type, payload } = action
 
     switch (type) {
-
+        case ADD_COMMENT:
+            return [...comments, payload]
     }
 
     return comments
