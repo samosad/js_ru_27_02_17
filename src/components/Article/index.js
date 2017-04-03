b8e2b1f849509b9ae4a0f9d8541d0f95c8d36241
Roman <kaboom.ua@gmail.com>
2017-04-03 11:14:49 +0300

    Update index.js

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -30,6 +30,7 @@ class Article extends Component {
 
     static contextTypes = {
         user: PropTypes.string,
+//тут и в других местах: лучше сделай декоратор либо компонент-обертку для локализации, чтоб не обращатся каждый раз к контексту, иначе потом тяжело будет что-либо поменять
         language: PropTypes.string
     }
 
